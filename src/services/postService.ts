8a62cffc85be162fe0719253fd95dce515162e6d
fandi-ahmad <fandi4160@gmail.com>
2024-12-05 14:36:20 +0800

    update service for post

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -3,9 +3,10 @@ import { createPostDTO } from "../interface/createPostDTO";
 
 export const PostService = {
   async createPost(data: createPostDTO) {
-    if (!data.content || !data.authorEmail) {
-      throw new Error("Content and author email are required! from service");
-    }
+    // if (!data.content || !data.authorEmail) {
+    //   throw new Error("Content and author email are required! from service");
+    // }
+    
 
     return await PostRepository.createPost(data);
   },
@@ -13,7 +14,7 @@ export const PostService = {
   async getPostById(id: number) {
     const post = await PostRepository.findPostById(id);
     if (!post) {
-      throw new Error("Post not found");
+      throw { status: 400, message: `Post with ID ${id} is not found` }
     }
     return post;
   },
@@ -22,11 +23,13 @@ export const PostService = {
     return await PostRepository.getAllPosts();
   },
 
-  async updatePost(id: number, data: any) {
-    return await PostRepository.updatePost(id, data);
+  async updatePost(id: number, content: string) {
+    await this.getPostById(id)
+    return PostRepository.updatePost(id, content);
   },
 
   async deletePost(id: number) {
+    await this.getPostById(id)
     return await PostRepository.deletePost(id);
   },
 };
