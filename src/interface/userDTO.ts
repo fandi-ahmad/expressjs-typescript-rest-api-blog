ff1369e559ed68a40879cc5456a2c37cb13bd8f7
fandi-ahmad <fandi4160@gmail.com>
2024-12-10 11:05:54 +0800

    modified DTO for user

--- src/interface/userDTO.ts
+++ src/interface/userDTO.ts
@@ -1,5 +1,13 @@
 export interface UserDTO {
   email: string
   username: string
+  name?: string
+  password: string
+  refreshToken?: string
+}
+
+export interface UserUpdateDTO {
+  email: string
+  username: string
   name: string
 }
